4af43649b5d1d24dac168c2d89a6f85614d24af0
Anthony <anthonyballugjr@gmail.com>
2020-04-23 19:49:46 +0800

    NEW: Sequelize project init

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
 
 const app = express();
 
+app.use(cors());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- src/config.js
+++ src/config.js
@@ -1,4 +1,35 @@
-// Place environment variables here...
-exports.DB = process.env.DB || 'this_is_the_db_conn_string';
-exports.JWT_TOKEN = process.env.JWT_TOKEN || 'this_is_the_jwt_token';
-exports.APP_NAME = 'NODE-EXPRESS';
+const environment = process.env.NODE_ENVIRONMENT || 'development';
+
+const dbConfig = {
+  development: {
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "operatorsAliases": false
+  },
+  test: {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  },
+  production: {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+}
+
+
+const config = {
+  dbConfig: dbConfig[environment]
+}
+
+module.exports = config
